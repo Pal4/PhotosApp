8dbbc7508bad63adfbd7548bd9a2b5a89bf7c8fe
Андрей Полунин <pal4.102rus@gmail.com>
2016-03-23 20:32:19 +0300

    APAlbumDetailViewController init

---
+++ PhotosApp/Controllers/Album/APAlbumDetailViewController.m
@@ -0,0 +1,13 @@
+//
+//  APAlbumDetailViewController.m
+//  PhotosApp
+//
+//  Created by Андрей Полунин on 3/23/16.
+//  Copyright © 2016 Andrey Polunin. All rights reserved.
+//
+
+#import "APAlbumDetailViewController.h"
+
+@implementation APAlbumDetailViewController
+
+@end
